ab9fa989a9e1d6c148e6d1370e59f262e34e333f
pharris050406 <p.harris050406@gmail.com>
2022-12-19 23:42:08 -0800

    added quickplace + discord integration

---
+++ src/minecraft/net/minecraft/Client.java
@@ -0,0 +1,24 @@
+package net.minecraft;
+
+public class Client {
+	private static final Client INSTANCE = new Client();
+	
+	public static final Client getInstance() {
+		return INSTANCE;
+	}
+	
+	private DiscordRP discordRP = new DiscordRP();
+	
+	public void init() {
+		discordRP.start();
+	}
+	
+	public void shutdown() {
+		discordRP.shutdown();
+	}
+	
+	public DiscordRP getDiscordRP() {
+		return discordRP;
+	}
+	
+}
